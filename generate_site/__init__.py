d0b237e11618b0cac6db6b0730e896c4027c9419
Ellen Marie Dash <me@duckie.co>
2019-12-16 08:43:48 -0500

    More work on site generation.

--- generate_site/__init__.py
+++ generate_site/__init__.py
@@ -1,5 +1,7 @@
+import datetime
 import http.client
 import io
+import json
 from pathlib import Path
 import sys
 from zipfile import ZipFile
@@ -37,15 +39,49 @@ def download_all(build_id):
     for os_name in os_names:
         download_data(build_id, os_name)
 
+
+def raw_data():
+    paths = Path('_data/source').glob('*.json')
+    return [json.loads(path.read_text()) for path in paths]
+
+
+def os_names():
+    return sorted([data['description'] for data in raw_data()])
+
+
+def normalized_data():
+    raw = raw_data()
+    data = {}
+    for chunk in raw:
+        data[chunk['description']] = chunk
+    return data
+
+
+def build_table():
+    data = normalized_data()
+    for name in os_names():
+        print(data[name])
+
+    exit()
+
+
 def main(argv):
     if len(argv) == 1:
         print("Usage: python3 -m generate_site BUILD_ID")
         sys.exit(1)
+
+    build_id = argv[1]
 
     Path('_site').mkdir()
     Path('_site/application.css').write_text(
         Path('src/application.css').read_text()
     )
 
-    build_id = argv[1]
     download_all(build_id)
+
+    date = datetime.datetime.utcnow().strftime('%b %d, %Y at %H:%M UTC')
+    table = build_table()
+    template = Path('src/index.html.template').read_text()
+    Path('_site/index.html').write_text(
+        template.replace('{{ date }}', date).replace('{{ table }}', table)
+    )
