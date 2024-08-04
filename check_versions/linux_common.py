5aeab07c5c9008c55593676a15f003877e540a88
Ellen Marie Dash <me@duckie.co>
2024-08-04 05:37:58 -0400

    migrate zypper mostly to output_parser.

--- check_versions/linux_common.py
+++ check_versions/linux_common.py
@@ -16,39 +16,41 @@ def _normalize_version(version):
 def parse_chunk(chunk):
     lines = chunk.split("\n")
 
-    chunk_info = {}
+    data = {}
     for line in lines:
         if ':' not in line:
             continue
         parts = line.split(':', 1)
         name = parts[0].strip()
         value = parts[1].strip()
-        chunk_info[name] = value
-    return chunk_info
+        data[name] = value
+
+    # pacman uses 'Package', apt/dnf/zypper use 'Name'
+    name = data.get('Package', data.get('Name'))
+
+    # DAMNIT ARCHLINUX.
+    if name == 'python':
+        name = 'python3'
+
+    version = _normalize_version(data['Version'])
+
+    print("{:20} {}".format(name, version))
+
+    return (name, {
+        'version': version,
+        'via': None,
+        })
+
+
+def parse_chunks(chunks):
+    parsed_chunks = [parse_chunk(chunk) for chunk in chunks]
+    return {name: data for (name, data) in parsed_chunks}
 
 
 def parse_common(output):
     output = output.replace("\r", "")
     chunks = output.strip().split("\n\n")
-
-    package_info = {}
-    for chunk in chunks:
-        data = parse_chunk(chunk)
-
-        # pacman uses 'Package', apt/dnf/zypper use 'Name'
-        name = data.get('Package', data.get('Name'))
-
-        # DAMNIT ARCHLINUX.
-        if name == 'python':
-            name = 'python3'
-
-        version = _normalize_version(data['Version'])
-        package_info[name] = {
-            'version': version,
-            'via': None,
-        }
-        print("{:20} {}".format(name, version))
-    return package_info
+    return parse_chunks(chunks)
 
 
 # raw `dnf info <packages>` output => {'pkg1': 'ver1', 'pkg2': 'ver2'}
@@ -58,24 +60,8 @@ def _parse_dnf(output):
     return parse_common(output)
 
 
-# raw `zypper info <packages>` output => {'pkg1': 'ver1', 'pkg2': 'ver2'}
-def _parse_zypper(output):
-    # Remove \r, collapse line continuations.
-    output = output.replace("\r", "").replace("\n    ", ' ')
-
-    root = ET.fromstring(output)
-    messages = root.findall("./message[@type='info']")
-    messages = [msg.text for msg in messages]
-    messages = list(filter(lambda msg: ':' in msg, messages))
-
-    output = "\n\n".join(messages)
-
-    return parse_common(output)
-
-
 def common_parse_info(pkgman, output):
     return {
         'common': parse_common,
         'dnf': _parse_dnf,
-        'zypper': _parse_zypper,
     }[pkgman](output)
