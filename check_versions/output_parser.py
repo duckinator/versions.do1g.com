3759830222529da05a06e69b2086f24fcbe470c1
Ellen Marie Dash <me@duckie.co>
2024-08-04 03:39:07 -0400

    Attempt to consolidate check_versions/output_parsers/* into check_versions/output_parser.py + check_versions/linux_common.py.

---
+++ check_versions/output_parser.py
@@ -0,0 +1,76 @@
+from .linux_common import common_parse_info
+
+class Distro:
+    OUTPUT_FORMAT = None
+    def parse_info(self, output):
+        return common_parse_info(self.OUTPUT_FORMAT, output)
+
+class ArchLinux(Distro):
+    OUTPUT_FORMAT = 'apt'
+    def info_command(self, packages):
+        # ArchLinux calls their python 3.x package 'python', not 'python3'.
+        # Unlike basically everything else I've ever encountered.
+        if 'python3' in packages:
+            packages_copy = packages.copy()
+            packages_copy[packages_copy.index('python3')] = 'python'
+        return 'pacman -Syi --noprogressbar {}'.format(' '.join(packages_copy))
+
+class Debian(Distro):
+    OUTPUT_FORMAT = 'apt'
+    def info_command(self, packages):
+        return 'apt-cache show {}'.format(' '.join(packages))
+
+class Fedora(Distro):
+    OUTPUT_FORMAT = 'dnf'
+    def info_command(self, packages):
+        packages = list(map(lambda x: x + '.x86_64', packages))
+        return 'dnf info --color=false {}'.format(' '.join(packages))
+
+class OpenSUSE(Distro):
+    OUTPUT_FORMAT = 'zypper'
+    def info_command(self, packages):
+        return 'zypper --xmlout info {}'.format(' '.join(packages))
+
+class FreeBSD(Distro):
+    def info_command(self, packages):
+        if 'clang' in packages:
+            packages[packages.index('clang')] = 'llvm'
+
+        return 'sudo pkg rquery "%n %v %dn=%dv" {}'.format(' '.join(packages))
+
+
+    def parse_info(output):
+        info = {}
+
+        output = output.strip()
+        lines = output.split("\n")
+        for line in lines:
+            pkg, version, dep = line.split(' ')
+            if dep.startswith(pkg):
+                realpkg, version = dep.split('=')
+            version = version.split('_')[0]
+            info[pkg] = {
+                'version': version,
+                'via': None,
+            }
+
+            # clang is provided by the 'llvm<version>' package, in FreeBSD
+            if pkg == 'llvm':
+                info['clang'] = {
+                    'version': version,
+                    'via': pkg,
+                }
+                #info['clang'] = '{}:via={}'.format(version, pkg)
+
+        return info
+
+DISTROS = {
+    'Arch Linux': ArchLinux,
+    'Manjaro Linux': ArchLinux,
+    'Debian': Debian,
+    'Ubuntu': Debian,
+    'Fedora Linux': Fedora,
+    'OpenSUSE Tumbleweed': OpenSUSE,
+    'OpenSUSE Leap': OpenSUSE,
+    'FreeBSD': FreeBSD,
+}
